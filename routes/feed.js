d9af2283db881fd1b048c7b3f27a3d443e7b15a9
ericodwr <ericodr@upnvj.ac.id>
2024-02-07 16:37:55 +0700

    first commit for rest api

---
+++ routes/feed.js
@@ -0,0 +1,36 @@
+const express = require('express');
+
+const { body } = require('express-validator');
+
+const feedController = require('../controllers/feed');
+const isAuth = require('../middleware/auth');
+
+const router = express.Router();
+
+router.get('/posts', isAuth, feedController.getPosts);
+
+router.post(
+  '/post',
+  [
+    body('title').trim().isLength({ min: 5 }),
+    body('content').trim().isLength({ min: 5 }),
+  ],
+  isAuth,
+  feedController.addPost,
+);
+
+router.get('/post/:postId', isAuth, feedController.getPost);
+
+router.put(
+  '/post/:postId',
+  [
+    body('title').trim().isLength({ min: 5 }),
+    body('content').trim().isLength({ min: 5 }),
+  ],
+  isAuth,
+  feedController.updatePost,
+);
+
+router.delete('/post/:postId', isAuth, feedController.deletePost);
+
+module.exports = router;
